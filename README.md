0a39e54637f284491679f076fb4d243104a1fddb
Rafael de Conde <GitHubRafael@gmail.com>
2022-07-07 14:57:46 -0300

    Teste de inversão da rotação do motor utilizando o arduino
    O intuito é testar a reação do ESC a inversão imediata do sentido do motor, se ele vai para o estado de break ou se ele vai inverter instantâneamente. Dependendo do comportamento, a programação dos comandos de movimento mudam.
    
    No momento do commit, o teste ainda não finalizado, por algum motivo o motor não rodou com o código e o circuito montados

--- README.md
+++ README.md
@@ -3,12 +3,16 @@ Repositório destinado para o controle dos códigos utilizados nos testes e estu
 
 Uma descrição mais completa do intuito do teste, curcuito e resultados estarão no readme de cada teste
 
-# Teste dos sensores ultrassônicos
+# Teste de controle do ESC sem utilização do potenciômetro
+
+Esse commit contém o código utilizado, também da forma na qual ele foi utilizado para realizar o teste do ESC sem potenciometro, o arquivo principal do sketch é o `testes-robo-locomotiva.ino`, que tem como base o arquivo `ControlESC_Turnigy.ino` da biblioteca [`Arduino-Control-Brushed-ESC`](https://github.com/tungstenexe/Arduino-Control-Brushed-ESC) refatorado para utilização dentro do Tera.
 
 ## Circuito eletrônico utilizado
 
-o circuito utilizado para esse teste foi apenas o arduino + protoborad + jumpers e o sensor infravermelho conectados.
+o circuito utilizado para esse teste foi o montado de acordo com o esquema contido nos arquivos `circuit.pdf` e `circuit_tinkercad.png` desse commit.
 
 ## Intuito e resultados
 
-O objetivo do teste era ver se os sensores estavam funcionando e os valores que eles mediam. Já foi realizada também a **calibração dos sensores**, no caso dos sensores infravermelho, apenas um deles estava com a sensibilidade descalibrada, fazendo com que ele se ativasse muito antes dos outros, porém utilizou-se uma have phillips para regular a sensibilidade do mesmo, deixam igual a dos outros. No final do teste e calibração, todos estavam se ativado ao mesmo tempo e funcionando de acordo com o planejado.
+O objetivo desse teste é estudar a reação do ESC agora sendo invertido totalmente pelo arduino, se a resposta será a mesma que obtemos quando testando o ESC com o radio controle. 
+
+Até o momento desse commit apenas uma tentativa foi realizada, devido a falta de tempo, ela foi interrompida no meio e não foi bem sucedida, o motor girou brevemente mas na maior parte do tempo ele não reagia, nem o led de funcionamento chegou a ascender, então atualmente é provavel que tenha algum problema no circuito ou na programação.
--- testes-robo-locomotiva.ino
+++ testes-robo-locomotiva.ino
@@ -1,17 +1,82 @@
-int sig{A0};
-long out{};
+#include "Bounce2.h"
+#include <Servo.h>
 
-void setup() 
+const int pino_led_funcionamento = 8;
+const int pino_botao_frente = 7;
+const int pino_botao_tras = 6;
+const int pino_botao_funcionamento = 5;
+const int pino_ESC = 3; 
+
+
+enum aceleracao : int
 {
-  Serial.begin(9600);
-  pinMode(sig,INPUT);
-  Serial.print("pino sig = ");
-  Serial.println(sig);
+	minima = 1080,
+	nula = 1480,
+	maxima = 1800
+};
+
+Bounce botao_frente = Bounce();
+Bounce botao_tras = Bounce();
+Bounce botao_funcionamento = Bounce();
+int aceleracao_atual = aceleracao::minima;
+boolean startReading = false;
+Servo ESC;
+int throttle{aceleracao::minima};
+
+void setup()
+{
+	Serial.begin(9600);
+	Serial.println("Started Setup........");
+	pinMode(pino_botao_funcionamento, INPUT_PULLUP); // Using Internal PULLUP resistor
+	pinMode(pino_botao_tras, INPUT_PULLUP);
+	pinMode(pino_botao_frente, INPUT_PULLUP);
+	pinMode(pino_led_funcionamento, OUTPUT);
+	botao_frente.attach(pino_botao_frente);
+	botao_frente.interval(5);
+	botao_tras.attach(pino_botao_tras);
+	botao_tras.interval(5);
+	botao_funcionamento.attach(pino_botao_funcionamento);
+	botao_funcionamento.interval(5);
+	ESC.attach(pino_ESC);
+	ESC.writeMicroseconds(aceleracao::nula);
+
+	digitalWrite(pino_led_funcionamento, HIGH);
+	delay(1000);
+	digitalWrite(pino_led_funcionamento, LOW);
 }
 
-void loop() 
+void loop()
 {
-  delay(100);
-  out = analogRead(sig);
-  Serial.println(out);
+
+	if (botao_funcionamento.fell())
+	{ // button pressed
+		if (!startReading)
+		{												// if not in reading mode
+			startReading = true;						// change to reading mode
+			digitalWrite(pino_led_funcionamento, HIGH); // on the onboard LED, for visual indicator
+			Serial.println("Start Sensor Read ----- ");
+		}
+		else
+		{												   // currently in reading mode, should stop reading and brake DC brushed motor
+			ESC.writeMicroseconds(aceleracao::nula); // brake DC brushed motor
+			startReading = false;						   // change to stop reading mode
+			throttle = aceleracao::nula;
+			digitalWrite(pino_led_funcionamento, LOW); // off the onboard LED, for visual indicator
+			Serial.println("Stop Sensor Read ----- ");
+		}
+	}
+
+	if (startReading)
+	{ // in reading mode
+		if(botao_frente.fell())
+		{
+			ESC.writeMicroseconds(aceleracao::maxima); 
+		}
+		else if(botao_tras.fell())
+		{
+			ESC.writeMicroseconds(aceleracao::minima);
+		}
+		
+
+	}
 }
