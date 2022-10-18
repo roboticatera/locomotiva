e419a50dd44f42a513daf69c340532bf6e8925fa
Enzo Fernandes <enzo.fernades@gmail.com>
2022-10-17 21:51:07 -0300

    Pinagem nova, de acordo com a placa do Denet

--- tests/geral/geral.ino
+++ tests/geral/geral.ino
@@ -1,53 +1,56 @@
 #include <Servo.h>
-#include "comandos_movimento.h"
 #include <Ultrasonic.h>
+#include "C:\Users\enzo_\Desktop\testes-robo-locomotinfraVermelhoa\comandos_movimento.h"
+using namespace Tera::movimento;
 
+// Sensor Infravermelho
+short pino_infraVermelho_tras_direita{3};
+short pino_infraVermelho_frente_direita{6};
+short pino_infraVermelho_frente_esquerda{9};
+short pino_infraVermelho_tras_esquerda{12};
 
+// Sensor Ultrassónico
+Ultrasonic ultrassonico_direita(4 /*trig*/, 5 /*echo*/);
+Ultrasonic ultrassonico_meio(7, 8);
+Ultrasonic ultrassonico_esquerda(10, 11);
 
-short pino_iv_1{2};
-short pino_iv_2{3};
-short pino_iv_3{4};
-short pino_iv_4{12};
-short pino_ESC{11};
+// ESC's
+short pino_ESC_esquerdo{A1};
+short pino_ESC_direito{A2};
+Servo ESC_esquerdo;
+Servo ESC_direito;
 
+// Medições
 int dist_meio{};
 int dist_esquerda{};
 int dist_direita{};
 bool na_linha{false};
 
-using namespace Tera::movimento;
-Ultrasonic ultra_direita(5,6);//Ultrasonic ultra1(trig,echo);
-Ultrasonic ultra_meio(7,8);
-Ultrasonic ultra_esquerda(9,10);
-// Servo ESC;
-//Servo ESC2; // ESC do motor 2
-
 void ataque()
 {
     // ESC.writeMicroseconds(aceleracao::maxima);//move com toda força pra frente
-    // Serial.println("Attack");
+    Serial.println("Attack");
     Serial.println("ataque: Motores indo pra frente");
 }
 
 void parar()
 {
     // ESC.writeMicroseconds(aceleracao::nula);//para o movimento do motor
-    // Serial.println("Parar");
+    Serial.println("Parar");
     Serial.println("parar: motores parados");
 }
 
-
 bool esta_na_linha()
 {
-    // os pontos de exclamação são necessários por que a lógica do iv
+    // os pontos de exclamação são necessários por que a lógica do infraVermelho
     // é que quando ele não está lendo nada, o sinal é 1 e quando ele está lendo
-    // alguma coisa o sinal é 0. o ponto de exclamação nega os valores, trocando o 0 -> 1 
+    // alguma coisa o sinal é 0. o ponto de exclamação nega os valores, trocando o 0 -> 1
     // e 1 -> 0.
-    bool iv1 = !digitalRead(pino_iv_1);
-    bool iv2 = !digitalRead(pino_iv_2);
-    bool iv3 = !digitalRead(pino_iv_3);
-    bool iv4 = !digitalRead(pino_iv_4);
-    return (iv1 || iv2 || iv3 || iv4);
+    bool infraVermelho_tras_direita = !digitalRead(pino_infraVermelho_tras_direita);
+    bool infraVermelho_frente_direita = !digitalRead(pino_infraVermelho_frente_direita);
+    bool infraVermelho_frente_esquerda = !digitalRead(pino_infraVermelho_frente_esquerda);
+    bool infraVermelho_tras_esquerda = !digitalRead(pino_infraVermelho_tras_esquerda);
+    return (infraVermelho_tras_direita || infraVermelho_frente_direita || infraVermelho_frente_esquerda || infraVermelho_tras_esquerda);
 }
 
 void girar_robo_esquerda()
@@ -70,7 +73,7 @@ void mudar_orientacao()
     {
         girar_robo_direita();
     }
-    else if ( (dist_esquerda != 0) && (dist_esquerda < 70) )
+    else if ((dist_esquerda != 0) && (dist_esquerda < 70))
     {
         girar_robo_esquerda();
     }
@@ -81,26 +84,27 @@ void mudar_orientacao()
     }
 }
 
-
 void setup()
 {
     Serial.begin(115200);
-    // ESC.attach(11);
-    pinMode(pino_iv_1, INPUT);
-    pinMode(pino_iv_2, INPUT);
-    pinMode(pino_iv_3, INPUT);
-    pinMode(pino_iv_4, INPUT);
+
+    ESC_esquerdo.attach(pino_ESC_esquerdo);
+    ESC_direito.attach(pino_ESC_direito);
+
+    pinMode(pino_infraVermelho_1, INPUT);
+    pinMode(pino_infraVermelho_2, INPUT);
+    pinMode(pino_infraVermelho_3, INPUT);
+    pinMode(pino_infraVermelho_4, INPUT);
 }
-
-
-
 
 void loop()
 {
     Serial.println("\n");
-    dist_meio = ultra_meio.read();
-    dist_esquerda = ultra_esquerda.read();
+
+    dist_meio = ultrassonico_meio.read();
+    dist_esquerda = ultrassonico_esquerda.read();
     dist_direita = ultra_direita.read();
+    
     Serial.print("dist_meio = ");
     Serial.println(dist_meio);
     Serial.print("dist_direita = ");
@@ -109,12 +113,13 @@ void loop()
     Serial.println(dist_esquerda);
     delay(500);
     na_linha = esta_na_linha();
+
     /*
      * Atualmente ele simplesmente para quando encontra a linha, é necessário
      * implementar uma função que faça com que ele saia da linha uma vez que encontrou ela
      */
-    
-    if( (dist_meio != 0 && dist_meio < 70) && (!na_linha) )
+
+    if ((dist_meio != 0 && dist_meio < 70) && (!na_linha))
     {
         ataque();
     }
@@ -123,8 +128,4 @@ void loop()
         parar();
         mudar_orientacao();
     }
-
-
 }
-
-
